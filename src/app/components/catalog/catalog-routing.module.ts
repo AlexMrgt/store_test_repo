e9de8e0b7f83ef37b5c87560c0a0f9c3cd588442
AlexMrgt <git.mrgt@gmail.com>
2021-10-25 00:22:49 +0500

    add routerLink example

--- src/app/components/catalog/catalog-routing.module.ts
+++ src/app/components/catalog/catalog-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {Route, RouterModule} from "@angular/router";
-import {ImportComponent} from "../import/import.component";
-import {ProjectsComponent} from "./components/projects/projects.component";
 
 const routes: Route[] = [
   // root link here: /catalog
